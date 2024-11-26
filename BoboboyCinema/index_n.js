6bcaae2e21f1983dc17ce4117cceb96a41f55b0e
Chong Hui <147372986+LinChongHui@users.noreply.github.com>
2024-11-26 14:43:35 +0800

    Delete BoboboyCinema directory

--- BoboboyCinema/index_n.js
+++
@@ -1,9 +0,0 @@
-document.addEventListener('DOMContentLoaded', () => {
-    const navToggle = document.getElementById('nav-toggle');
-    const navContainer = document.getElementById('nav-container');
-
-    navToggle.addEventListener('click', () => {
-        navContainer.classList.toggle('hidden');
-        navContainer.classList.toggle('visible');
-    });
-});
